8910bb903c8e6616a4bae523f85f712e978e34b3
bonbonpa <sphinspf@gmail.com>
2018-04-09 17:49:29 +0700

    update : learn book

---
+++ chapter2/5.Bootstrap4/vue-bt/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <alert :title="title" :text="text" :type="type"></alert>
+  </div>
+</template>
+
+<script>
+import Alert from "./components/Alert";
+export default {
+  name: "app",
+  components: {
+    Alert
+  },
+  data() {
+    return {
+      title: "Vue Bootstrap Component",
+      text: "Isn't it easy ?",
+      randomValue: Math.ceil(Math.random() * 100)
+    };
+  },
+  computed: {
+    type() {
+      if (this.randomValue % 3 === 0) {
+        return "info";
+      } else if (this.randomValue % 2 === 0) {
+        return "danger";
+      } else if (this.randomValue % 5 === 0) {
+        return "warning";
+      }
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ example_pj/BOOK_STORE.md
@@ -0,0 +1,38 @@
+
+Page
+- Home
+- Last Added
+- Categories
+- DMCA
+- Contact
+- Register
+- Login
+- Book Detail
+
+Feature
+- Add Book
+- Edit Book
+- Delete Book
+- Search Book
+    - Title
+    - Tag
+    - Author
+
+## Page
+
+### Book Detail
+
+- Book Name : Pro Apache JMeter: Web Application Performance Testing
+- Edition : 1st Edition
+- ISBN : 1484229606
+- Author Name : Sai Matam; Jagdeep Jain
+- Category : Programming & IT
+- Format : PDF
+- Pages : 335 Pages
+- Book Description : ...........
+- Preview link : url_preview
+- Download link : url_download
+- Buy It link : url_buyit
+- Post By : Ebook Download
+- Posted on : 8/21/2017 3:06:07 PM
+- Tag : Apache, Web
